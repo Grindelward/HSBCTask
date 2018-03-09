bb691152bb9f3ff435103a7c768189d27018037f
Grindelward <przemyslaw.nowicki93@gmail.com>
2018-03-09 18:58:18 +0100

    init repo

---
+++ src/main/java/com/prznow/hsbc/SimpleTwitterApplication.java
@@ -0,0 +1,12 @@
+package com.prznow.hsbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleTwitterApplication.class, args);
+	}
+}
